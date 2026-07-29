4b1730a995695ab42cd067a185e8d619d2a860f9
apheleledube10-max <apheleledube10@gmail.com>
2026-07-30 01:05:23 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,37 @@
+// ======================================
+// MKHONKOSI ELECTRICAL SERVICES
+// HERO SLIDESHOW
+// ======================================
+
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+function showSlide(index){
+
+    slides.forEach((slide)=>{
+
+        slide.classList.remove("active");
+
+    });
+
+    slides[index].classList.add("active");
+
+}
+
+function nextSlide(){
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+
+        currentSlide = 0;
+
+    }
+
+    showSlide(currentSlide);
+
+}
+
+// Start slideshow
+setInterval(nextSlide, 5000);
